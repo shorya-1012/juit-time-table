072f8501f537f1bcbae010bac2c2b7533f85f0e4
SurajKharkwal <kharkwalsuraj13@gmail.com>
2024-03-01 13:33:15 +0530

    Edit: Few ui chages in toast

---
+++ app/test/ToastDemo.tsx
@@ -0,0 +1,27 @@
+"use client"
+import * as React from 'react';
+import * as Toast from '@radix-ui/react-toast';
+import './styles.css';
+
+const ToastDemo = () => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Toast.Provider swipeDirection="right">
+      <Toast.Root className="ToastRoot" open={true} onOpenChange={setOpen}>
+        <div className=''>
+          <Toast.Title className="ToastTitle">
+            <h1 className='text-red-500 font-semibold'>Sorry, No Data Found</h1>
+          </Toast.Title>
+          <Toast.Description asChild>
+            <p className='text-white font-extralight'>Please Check, Course Name & Batch Name again ...</p>
+          </Toast.Description>
+        </div>
+      </Toast.Root>
+      <Toast.Viewport className="ToastViewport" />
+    </Toast.Provider>
+  );
+};
+
+
+export default ToastDemo;
---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import ToastDemo from './ToastDemo'
+
+const page = () => {
+    return (
+        <div className='w-full h-full'>
+            <ToastDemo />
+        </div>
+    )
+}
+
+export default page
